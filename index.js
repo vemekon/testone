f2fba305a255354b545e944f9ad9d79b3d14cd1f
samuel112 <senaymokie@gmail.com>
2022-04-14 20:34:28 +0100

    GM

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
