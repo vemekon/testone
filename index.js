5d5708417948b170f266731d925dde22b7b25894
samuel112 <senaymokie@gmail.com>
2022-04-14 19:45:17 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("App is running ok");
+});
+
+const CONNECTION_URL =
+  "mongodb+srv://admin:aa123456@cluster0.qgmjq.mongodb.net/AstonVisit";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`did not connect`));
